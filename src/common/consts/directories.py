f4631f1eb05549202a39d1ef112a2e6a19c24b1d
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2024-02-02 09:40:07 +0100

    Add basic consts

---
+++ src/common/consts/directories.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[3]
+
+DATA_DIR = ROOT_DIR / "data"
+ENVIRONMENTS_DIR = ROOT_DIR / "environments"
+DOCKER_DIR = ROOT_DIR / "docker"
+NOTEBOOKS_DIR = ROOT_DIR / "notebooks"
+TESTS_DIR = ROOT_DIR / "tests"
+SOURCE_DIR = ROOT_DIR / "src"
