40582f77fd000788554368094a25adf820da218b
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2024-02-06 13:27:15 +0100

    Implement retry logic to guard againa random Pinecone errors

--- src/vector_store.py
+++ src/vector_store.py
@@ -5,6 +5,7 @@ from typing import List
 
 from langchain_core.documents import Document
 from pinecone import Index, Pinecone, PodSpec
+from retry import retry
 from tqdm import tqdm
 
 from src.common.utils.logger import get_logger, timed
@@ -73,7 +74,7 @@ class VectorStore:
         for i in tqdm(range(0, len(data), batch_size)):
             batch_data = data[i : i + batch_size]
             batch_embeddings = embeddings[i : i + batch_size]
-            self._upsert_batch(index=index, datadata=batch_data, embeddings=batch_embeddings)
+            self._upsert_batch(index=index, data=batch_data, embeddings=batch_embeddings)
             expected_vectors += len(batch_data)
 
             while index.describe_index_stats()["total_vector_count"] < expected_vectors:
@@ -83,17 +84,21 @@ class VectorStore:
 
         _logger.info(f"A total of {len(data)} documents upserted into Pinecone index.\n{index.describe_index_stats()}")
 
-    def _upsert_batch(self, index: Index, datadata: List[Document], embeddings: List[List[float]]) -> None:
+    @retry(tries=10, delay=5, logger=_logger)
+    def _upsert_batch(self, index: Index, data: List[Document], embeddings: List[List[float]]) -> None:
         """
         Helper function to upsert a batch of documents and their embeddings.
 
         Args:
             index (Index): The Pinecone index to upsert into.
-            datadata (List[Document]): The batch of documents to upsert.
+            data (List[Document]): The batch of documents to upsert.
             embeddings (List[List[float]]): The embeddings corresponding to the batch of documents.
         """
-        vectors_to_upsert = self._prepare_vectors_for_upsert(datadata, embeddings)
-        index.upsert(vectors=vectors_to_upsert)
+        vectors_to_upsert = self._prepare_vectors_for_upsert(data, embeddings)
+        try:
+            index.upsert(vectors=vectors_to_upsert)
+        except Exception as e:
+            raise Exception(f"Failed to upsert vectors into Pinecone index: {e}")
 
     def _prepare_vectors_for_upsert(
         self, batch_data: List[Document], batch_embeddings: List[List[float]]
